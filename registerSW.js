d7aa5eb1b8f25da61af96a7505684d27ace951e0
loqmanali <grahyy1@gmail.com>
2024-10-01 18:35:32 +0700

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('https://github.com/loqmanali/vite-PWA/sw.js', { scope: 'https://github.com/loqmanali/vite-PWA/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/vite-PWA/sw.js', { scope: '/vite-PWA/' })})}
